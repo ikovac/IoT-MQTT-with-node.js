a8b461e2b69047f842c76f62e2dd717487ac038e
Ivo Kovačević <ikovacevic96k@gmail.com>
2018-05-21 17:30:06 +0200

    Add files via upload

---
+++ sketch_arduino_server.ino
@@ -0,0 +1,52 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <Wire.h>
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+float h;
+float t;
+
+void readTempHum();
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+}
+
+void loop() {
+  readTempHum();
+}
+
+
+void readTempHum() {
+
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)) {
+    Serial.print("-1");
+    return;
+  }
+  Serial.print("Humidity: ");
+  Serial.println(h);
+  Serial.print("Temperature: ");
+  Serial.println(t);
+  
+}
+
+
